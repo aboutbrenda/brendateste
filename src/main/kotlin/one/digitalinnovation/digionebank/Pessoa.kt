8424981ca27c8021865e811205f08596e6667ff2
aboutbrenda <brenda-gabriel@hotmail.com>
2021-06-30 15:58:12 -0300

    uptaded to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,12 +4,15 @@ class Pessoa {
     var nome: String = "Brenda"
     var cpf: String = "162.162.162-62"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main () {
     val brenda = Pessoa()
 
-    println(brenda.nome)
-    println(brenda.cpf)
+    println(brenda.pessoaInfo())
 
 }
