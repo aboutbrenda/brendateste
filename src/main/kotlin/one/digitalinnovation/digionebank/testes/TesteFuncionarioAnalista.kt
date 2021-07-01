ae8e4bf06687ee66d6af28ef67da62f455d36f36
aboutbrenda <brenda-gabriel@hotmail.com>
2021-06-30 23:27:02 -0300

    created tests and report printer

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioAnalista.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Analista
+
+fun main () {
+    val joao = Analista("João Pedro","162.162.162-62", 2000.0)
+ImprimeRelatorioFuncionario.imprime(joao)
+}
