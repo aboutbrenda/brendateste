d2b7577128658e7daaae96eae23714c4668e6206
aboutbrenda <brenda-gabriel@hotmail.com>
2021-07-01 14:36:17 -0300

    finalizado

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -7,6 +7,8 @@ import one.digitalinnovation.digionebank.Pessoa
 import java.math.BigDecimal
 
 fun main () {
-    val maria = Gerente("Maria do Carmo","162.162.162-62", 5000.0)
+    val maria = Gerente("Maria do Carmo","162.162.162-62", 5000.0, "123")
 ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
