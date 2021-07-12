36123dd3047c3ec3065cfc801ad503718686dec2
aboutbrenda <brenda-gabriel@hotmail.com>
2021-07-12 13:07:47 -0300

    uptated test list, extract employee to separated file and created test set

---
+++ api-collections/src/test/kotlin/me/brenda/Funcionario.kt
@@ -0,0 +1,12 @@
+package me.brenda
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String) {
+    override fun toString(): String =
+        """
+            Nome: $nome
+            Salário: $salario
+        """.trimIndent()
+}
